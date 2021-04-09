c64e6a6a429991b8e59b8ea5812d739c192d7d53
Sam Parton <tony8tr@hotmail.com>
2021-04-10 00:16:12 +0100

    Export container type
    Signed-off-by: Sam Parton <tony8tr@hotmail.com>

--- packages/App/src/index.ts
+++ packages/App/src/index.ts
@@ -2,4 +2,4 @@ export * from './App'
 export * from './ServiceProvider';
 export * from './Config/ConfigRepository';
 export * from './Exceptions/FailedToBindException';
-export {injectable, inject} from 'tsyringe';
+export {injectable, inject, DependencyContainer } from 'tsyringe';
