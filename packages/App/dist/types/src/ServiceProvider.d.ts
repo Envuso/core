290c64b96c3bf460c01587ff6a4b6bece7d451cf
Sam Parton <tony8tr@hotmail.com>
2021-04-09 14:48:48 +0100

    build assets
    Signed-off-by: Sam Parton <tony8tr@hotmail.com>

--- packages/App/dist/types/src/ServiceProvider.d.ts
+++ packages/App/dist/types/src/ServiceProvider.d.ts
@@ -1,13 +1,14 @@
 import { App } from "@src/App";
+import { ConfigRepository } from "@src/Config/ConfigRepository";
 export declare abstract class ServiceProvider {
     /**
      * Register any services to the container
      */
-    abstract register(app: App): Promise<void>;
+    abstract register(app: App, config: ConfigRepository): Promise<void>;
     /**
      * Should not be used to bind any services to the container
      * This method will have access to all other services,
      * since it has been called after register()
      */
-    abstract boot(app: App): Promise<void>;
+    abstract boot(app: App, config: ConfigRepository): Promise<void>;
 }
