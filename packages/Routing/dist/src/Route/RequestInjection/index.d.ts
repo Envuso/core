acde8572ac067f731b7c83dd469e9e0786f9687a
Sam Parton <tony8tr@hotmail.com>
2021-04-11 04:14:45 +0100

    package: Build & change class-transformer to regular dep

--- packages/Routing/dist/src/Route/RequestInjection/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from './MethodParameterDecorator';
-export * from './DataTransferObjectParam';
-export * from './RequestParam';
-export * from './RouteQueryParam';
-export * from './RequestBodyParam';
-export * from './RouteParameterParam';
-export * from './RequestHeadersParam';
