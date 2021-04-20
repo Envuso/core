034f93c5ba10c6f4f387609e3011abe5baf0256c
Sam Parton <sam@idevelopthings.com>
2021-04-21 00:36:00 +0100

    Attempting to fix issue with auto container resolution in controllers

--- packages/Storage/dist/src/StorageProviderContract.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.StorageProviderContract = void 0;
-class StorageProviderContract {
-}
-exports.StorageProviderContract = StorageProviderContract;
-//# sourceMappingURL=StorageProviderContract.js.map
