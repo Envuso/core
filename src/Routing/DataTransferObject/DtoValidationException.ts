12a1240096d0b0f706761b54449764394b7f020e
Sam Parton <sam@idevelopthings.com>
2021-04-26 01:40:11 +0100

    Make DtoValidationException extend exception

--- src/Routing/DataTransferObject/DtoValidationException.ts
+++ src/Routing/DataTransferObject/DtoValidationException.ts
@@ -1,12 +1,18 @@
 import {ValidationError} from "class-validator";
+import {Exception} from "../../Common";
 
-export class DtoValidationException extends Error {
+export class DtoValidationException extends Exception {
 	private _validationErrors: ValidationError[];
 
 	constructor(validationErrors: ValidationError[]) {
 		super('Failed to validate class properties');
 
 		this._validationErrors = validationErrors;
+
+		this.response = {
+			message : this.message,
+			errors  : this._validationErrors
+		};
 	}
 
 }
